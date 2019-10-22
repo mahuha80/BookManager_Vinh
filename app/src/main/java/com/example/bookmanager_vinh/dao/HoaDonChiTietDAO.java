2f747d32a70c5719082187b6e4dbfa150d125303
mahuha80 <54239463+mahuha80@users.noreply.github.com>
2019-10-23 00:27:32 +0700

    chuc nang tinh doanh so

--- app/src/main/java/com/example/bookmanager_vinh/dao/HoaDonChiTietDAO.java
+++ app/src/main/java/com/example/bookmanager_vinh/dao/HoaDonChiTietDAO.java
@@ -2,9 +2,11 @@ package com.example.bookmanager_vinh.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.bookmanager_vinh.database.DatabaseHelper;
+import com.example.bookmanager_vinh.model.HoaDon;
 import com.example.bookmanager_vinh.model.HoaDonChiTiet;
 
 import java.text.SimpleDateFormat;
@@ -35,6 +37,19 @@ public class HoaDonChiTietDAO {
         contentValues.put("masach", hoaDonChiTiet.getSach().getMasach());
         contentValues.put("soluong", hoaDonChiTiet.getSoLuongMua());
         return db.update(TABLE_NAME, contentValues, "maHDCT=?", new String[]{hoaDonChiTiet.getMaHDCT() + ""});
+    }
+
+    public double getDoanhThuTheoNgay(String ngaymua) {
+        double doanhThu = 0;
+        String sSQL = "select sum(tongtien) from(select sum(Sach.giabia* HoaDonChiTiet.soLuong) as 'tongtien' from HoaDon inner join HoaDonChiTiet on HoaDon.mahoadon=HoaDonChiTiet.mahoadon inner join Sach on Sach.masach=HoaDonChiTiet.masach where strftime('%d', HoaDon.ngaymua)=?  group by HoaDonChiTiet.masach)tmp";
+        Cursor c = db.rawQuery(sSQL, new String[]{ngaymua});
+        c.moveToFirst();
+        while (c.isAfterLast() == false) {
+            doanhThu = c.getDouble(0);
+            c.moveToNext();
+        }
+        c.close();
+        return doanhThu;
     }
 
 }
--- app/src/main/java/com/example/bookmanager_vinh/ui/ThongKeActivity.java
+++ app/src/main/java/com/example/bookmanager_vinh/ui/ThongKeActivity.java
@@ -3,14 +3,29 @@ package com.example.bookmanager_vinh.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bookmanager_vinh.R;
+import com.example.bookmanager_vinh.dao.HoaDonChiTietDAO;
+
+import java.util.Calendar;
 
 public class ThongKeActivity extends AppCompatActivity {
+    TextView tvHomNay, tvThangNay, tvNamNay;
+    HoaDonChiTietDAO hoaDonChiTietDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_ke);
+        tvHomNay = findViewById(R.id.tvHomNay);
+        tvThangNay = findViewById(R.id.tvThangNay);
+        tvNamNay = findViewById(R.id.tvNamNay);
+        hoaDonChiTietDAO = new HoaDonChiTietDAO(this);
+        Toast.makeText(this, Calendar.getInstance().get(Calendar.DAY_OF_MONTH)+"", Toast.LENGTH_SHORT).show();
+        double doanhthungay= hoaDonChiTietDAO.getDoanhThuTheoNgay("Doanh thu theo ngày:" + hoaDonChiTietDAO.getDoanhThuTheoNgay("" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH)));
+        Toast.makeText(this, doanhthungay+"", Toast.LENGTH_SHORT).show();
+
     }
 }
